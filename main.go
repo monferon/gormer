a305907c2b5ae862b54df8ac66b9eb04cd9cdf51
Monnoroch <sidelnikov.ns@gmail.com>
2022-03-16 22:42:02 +0300

    add CRUD with gin and gorm

--- main.go
+++ main.go
@@ -1,100 +1,51 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
-	"github.com/jackc/pgtype"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
-	"log"
-	"net/http"
+	"github.com/gin-gonic/gin"
+	"gormer/controllers"
+	"gormer/database"
 )
 
-type User struct {
-	ID       int    `gorm:"column:Id" gorm:"primaryKey"`
-	Username string `gorm:"column:Username"`
-	Password string `gorm:"column:Password"`
-	Role     int    `gorm:"column:Role"`
-}
+//type User struct {
+//	ID       int    `gorm:"column:Id" gorm:"primaryKey"`
+//	Username string `gorm:"column:Username"`
+//	Password string `gorm:"column:Password"`
+//	Role     int    `gorm:"column:Role"`
+//}
+//
+//type Test struct {
+//	ID       int `gorm:"primaryKey"`
+//	test1    string
+//	text2    string
+//	datetime pgtype.Date
+//}
 
-type Test struct {
-	ID       int `gorm:"primaryKey"`
-	test1    string
-	text2    string
-	datetime pgtype.Date
-}
-
-type Queue struct {
-	ID        int
-	Name      string
-	TestField string
-}
-
-type Sea struct {
-	ID   int
-	Path string
-	Qp   int
-	Tt   string
-	Pp   int
-}
-
-type Inter interface {
-}
-
-func (u User) TableName() string {
-	return "User"
-}
-
-func (u Test) TableName() string {
-	return "testTable"
-}
-
-func TestHadnler(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func QueueHandler(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func SeaHandler(w http.ResponseWriter, r *http.Request) {
-
-}
+//type Queue struct {
+//	ID        int
+//	Name      string
+//	TestField string
+//}
+//
+//type Sea struct {
+//	ID   int
+//	Path string
+//	Qp   int
+//	Tt   string
+//	Pp   int
+//}
 
 func main() {
+	r := gin.Default()
+	database.Connect()
 
-	//var user1 User
-	//var users []User
-	//var test Test
-	//var inte Inter
-	//dsn := "host=localhost user=postgres password=qwerty dbname=postgres port=5432"
-
-	//Init
-	r := mux.NewRouter()
-	r.HandleFunc("/", TestHadnler)
-	r.HandleFunc("/queue", QueueHandler)
-	r.HandleFunc("/sea", SeaHandler)
-	http.Handle("/", r)
-	db, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
-	if err != nil {
-		log.Fatal(err)
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/books", controllers.FindBooks)
+		v1.POST("/books", controllers.CreateBook)
+		v1.GET("/books/:id", controllers.FindBook)
+		v1.PATCH("/books/:id", controllers.UpdateBook)
+		v1.DELETE("/books/:id", controllers.DeleteBook)
 	}
 
-	//INSERT
-	//us := User{Username: "ustat", Password: "pwd", Role: 1}
-	//result := db.Create(&us)
-	//fmt.Println(result.RowsAffected)
-	//db.Select("Username", "Password", "Role").Create(&us)
-	//db.Select('')
-	//firstUser := User{}
-	//db.First(&firstUser)
-
-	//SELECT
-	//result2 := db.Find(&users)
-	//fmt.Println(result2.RowsAffected)
-	//results := db.Where("'Username' = ?", "Ivan").First(&user1)
-	//fmt.Println(results.RowsAffected)
-	//
-	//resultTest := db.Where("text2 = ?", "2").Find(&inte)
-	//fmt.Println(resultTest.RowsAffected)
-	//fmt.Println("agaga")
+	r.Run()
 }
